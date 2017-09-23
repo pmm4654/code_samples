0fdb9370e10284bff63bd3ee7cac43346db8833c
Marco Fatica <marco.fatica@gmail.com>
2017-09-23 10:56:12 -0400

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,30 +2,27 @@ import math
 import timeit
 
 class PrimeNumbers():
-	def __init__(self):
-		self.prime_list = [2]
+    def __init__(self):
+        self.prime_list = []
+        self.memo = {}
 
-	def get_prime_numbers(self, up_to_x):
-		my_list = [x for x in range(3, up_to_x + 1, 2)]
+    def get_prime_numbers(self, up_to_x):
+        my_list = [x for x in range(3, up_to_x + 1, 2)]
 
-		for prime_candidate in my_list:
-			is_prime = self.check_is_prime(prime_candidate)
-			if is_prime:
-				self.prime_list.append(prime_candidate)
+        for prime_candidate in my_list:
+            is_prime = self.check_is_prime(prime_candidate)
+            if is_prime:
+                self.prime_list.append(prime_candidate)
+                
+        return self.prime_list
 
-		#print (self.prime_list)
-
-	def check_is_prime(self, check_for_prime):
-		for number in range(2, check_for_prime):
-			if (check_for_prime % number) == 0:
-				return False
-		return True
-
-# a = range(1, 10)
-# [x for x in a if x not in [2, 3, 7]]
+    def check_is_prime(self, check_for_prime):
+        for number in range(2, check_for_prime // 2):
+            if (check_for_prime % number) == 0:
+                return False
+        return True
 
 prime = PrimeNumbers()
-prime.get_prime_numbers(100)
-prime.prime_list
+print(prime.get_prime_numbers(10))
 
 timeit.timeit('PrimeNumbers().get_prime_numbers(10000)', number = 5, setup='from __main__ import PrimeNumbers')
