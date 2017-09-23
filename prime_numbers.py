4be16088d89e12ed630017af0caf005ccd6b6b7e
Marco Fatica <marco.fatica@gmail.com>
2017-09-23 10:49:23 -0400

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,4 +1,6 @@
 import math
+import timeit
+
 class PrimeNumbers():
 	def __init__(self):
 		self.prime_list = [2]
@@ -11,7 +13,7 @@ class PrimeNumbers():
 			if is_prime:
 				self.prime_list.append(prime_candidate)
 
-		print (self.prime_list)
+		#print (self.prime_list)
 
 	def check_is_prime(self, check_for_prime):
 		for number in range(2, check_for_prime):
@@ -19,5 +21,11 @@ class PrimeNumbers():
 				return False
 		return True
 
+# a = range(1, 10)
+# [x for x in a if x not in [2, 3, 7]]
+
 prime = PrimeNumbers()
 prime.get_prime_numbers(100)
+prime.prime_list
+
+timeit.timeit('PrimeNumbers().get_prime_numbers(10000)', number = 5, setup='from __main__ import PrimeNumbers')
